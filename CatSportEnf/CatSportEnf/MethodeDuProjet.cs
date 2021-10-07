aad768433da878ed6ca59718db5349c63afdf022
NathLeem <91312993+NathLeem@users.noreply.github.com>
2021-10-07 15:49:01 +0200

    upload du programme complet

---
+++ CatSportEnf/CatSportEnf/MethodeDuProjet.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CatSportEnf
+{
+    public struct MethodeDuProjet
+    {
+        public void CatSportEnf(int Age,out string Cat)
+        {
+            if (Age <= 5)
+            {
+                Cat = "Pas de catégorie";
+            }
+            else if (Age <= 7)
+            {
+                Cat = "Poussin";
+            }
+            else if (Age <= 9)
+            {
+                Cat = "Pupille";
+            }
+            else if (Age <= 11)
+            {
+                Cat = "Minime";
+            }
+            else if (Age <= 14)
+            {
+                Cat = "Cadet";
+            }
+            else
+            {
+                Cat = "Pas de catégorie";
+            }
+        }
+    }
+}
---
+++ CatSportEnf/CatSportEnf/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace CatSportEnf
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int Age;
+            string Cat;
+
+            Console.WriteLine("Entrez votre age:");
+            Age = Convert.ToInt32(Console.ReadLine());
+
+            MethodeDuProjet MesOutils = new MethodeDuProjet();
+            MesOutils.CatSportEnf(Age, out Cat);
+
+            Console.WriteLine($"Vous êtes dans la catégorie: {Cat}");
+            Console.ReadLine();
+        }
+    }
+}
